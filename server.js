dccef323bd0d300c1c7402cc5ef91cd432fde7aa
Rachelforce <art.piskov@gmail.com>
2022-12-29 19:09:27 +0400

    Update server.js

--- server.js
+++ server.js
@@ -75,6 +75,10 @@ app.get('/files',function (req, res, next){
     });
 })
 
+app.get('/',function (req, res, next){
+    res.send("Server is running")
+})
+
 app.listen(8000, () => {
     console.log('App is running on port 8000')
 });
